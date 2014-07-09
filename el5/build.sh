4961036ce5eac93cc3638805ff8c935003d778e5
Darren Beck <dmbeck@gmail.com>
2014-07-09 10:50:35 +0800

    Added support for Enterprise Linux 5 systems, not all plugins are available using the spec file requirements but the majority are

---
+++ el5/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install tools required to build the rpms
+yum install -y rpm-build yum-utils rpmdevtools
+
+# Install EPEL
+rpm -ivh http://mir01.syntis.net/epel/5/i386/epel-release-5-4.noarch.rpm
+
+# Go to /tmp to get and extract collectd source base
+cd /tmp
+wget -c http://collectd.org/files/collectd-5.4.1.tar.bz2
+
+# Extract source
+tar xvf collectd-5.4.1.tar.bz2
+
+# Copy sources in build directory
+cp collectd-5.4.1.tar.bz2 /usr/src/redhat/SOURCES/
+
+# Fix the specfile to match collected version
+sed -i s/5.4.0/5.4.1/ collectd-5.4.1/contrib/redhat/collectd.spec
+
+# Install dependencies of the spec file
+yum install -y $(egrep "^BuildRequires" collectd-5.4.1/contrib/redhat/collectd.spec | cut -d ':' -f 2 | tr -d ' \t' | sed 's/,/\n/' | uniq | sed 's/$/ /' | tr -d '\n')
+
+# Create RPMS
+rpmbuild -bb collectd-5.4.1/contrib/redhat/collectd.spec
+
+# Copy RPMs back to the host
+cp -rv /usr/src/redhat/RPMS/ /vagrant
+
