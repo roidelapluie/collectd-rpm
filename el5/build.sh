94d873893796609cf8e0240c68610e51638ae246
MrSecure <MrSecure@users.noreply.github.com>
2015-05-16 10:25:02 -0500

    blind attempt to build 5.4.2 packages

--- el5/build.sh
+++ el5/build.sh
@@ -8,22 +8,22 @@ rpm -ivh http://mir01.syntis.net/epel/5/i386/epel-release-5-4.noarch.rpm
 
 # Go to /tmp to get and extract collectd source base
 cd /tmp
-wget -c http://collectd.org/files/collectd-5.4.1.tar.bz2
+wget -c http://collectd.org/files/collectd-5.4.2.tar.bz2
 
 # Extract source
-tar xvf collectd-5.4.1.tar.bz2
+tar xvf collectd-5.4.2.tar.bz2
 
 # Copy sources in build directory
-cp collectd-5.4.1.tar.bz2 /usr/src/redhat/SOURCES/
+cp collectd-5.4.2.tar.bz2 /usr/src/redhat/SOURCES/
 
 # Fix the specfile to match collected version
-sed -i s/5.4.0/5.4.1/ collectd-5.4.1/contrib/redhat/collectd.spec
+sed -i s/5.4.0/5.4.2/ collectd-5.4.2/contrib/redhat/collectd.spec
 
 # Install dependencies of the spec file
 yum install -y $(egrep "^BuildRequires" collectd-5.4.1/contrib/redhat/collectd.spec | cut -d ':' -f 2 | tr -d ' \t' | sed 's/,/\n/' | uniq | sed 's/$/ /' | tr -d '\n')
 
 # Create RPMS
-rpmbuild -bb collectd-5.4.1/contrib/redhat/collectd.spec
+rpmbuild -bb collectd-5.4.2/contrib/redhat/collectd.spec
 
 # Copy RPMs back to the host
 cp -rv /usr/src/redhat/RPMS/ /vagrant
